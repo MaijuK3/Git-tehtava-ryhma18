be27ba603fe77560f8777475ffaee71d405fc106
MaijuK <142224158+MaijuK3@users.noreply.github.com>
2023-12-01 11:36:55 +0200

    Luotu attribuutit ja naytaTiedot-metodi

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,9 @@ public class Main {
 //Auto-luokka:
 class Auto {
 	// auton ominaisuudet:
-	
+	public String merkki;
+	public String malli;
+	public int bensanMaara;	
 	
 	
 	// oletusmuodostin:
@@ -24,21 +26,25 @@ class Auto {
 	
 	
 	// auton toiminnot:
-	// Jarruta-metodi:
+	// jarruta-metodi:
 	
 	
 	
-	// Kiihdyta-metodi:
+	// kiihdyta-metodi:
 	
 	
 	
-	// Tankkaa-metodi:
+	// tankkaa-metodi:
 	
 	
 	
-	// NaytaTiedot-metodi:
-	
-	
+	// naytaTiedot-metodi:
+	public void NaytaTiedot() {
+		System.out.println("*** Auton tiedot ***");
+		System.out.println("Merkki: " + merkki);
+		System.out.println("Malli: " + malli);
+		System.out.println("Bensan määrä: " + bensanMaara);
+	}	
 	
 	
 } // Auto-luokka loppuu
